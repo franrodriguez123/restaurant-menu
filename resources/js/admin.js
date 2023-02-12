5f84c4d5865082bf740197130bc635999ee10c8e
franrodriguez123 <118189594+franrodriguez123@users.noreply.github.com>
2023-02-12 18:08:55 +0000

    Script para la subida del logo

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,11 +1,35 @@
-import './bootstrap';
+class UploadPreview
+{
+	constructor(input)
+	{
+		let wrapper = document.createElement('DIV');
+		let img = document.createElement('IMG');
+		
+		wrapper.classList.add('upload-preview');
+        wrapper.classList.add('none');
+		wrapper.append(img);
+		
+		input.parentNode.insertBefore(wrapper, input)
+		
+		input.addEventListener('change', event => {
+			const [file] = input.files;
+			
+			wrapper.classList.remove('none');
+			
+			if(file)
+			{
+    			img.src = URL.createObjectURL(file);
+			}
+		});
+		
+		if(input.dataset.file)
+		{
+			wrapper.classList.remove('none');
+			img.src = '/img/logo/' + input.dataset.file;
+		}
+	}
+}
 
-import * as bootstrap from 'bootstrap'
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
-console.log('ESTAS EN ADMIN')
+document.querySelectorAll('input[type="file"]').forEach(element => {
+	new UploadPreview(element);
+});
