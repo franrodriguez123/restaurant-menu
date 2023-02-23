b002000cfe6b541617baa55aec7f40bace36fc14
franrodriguez123 <118189594+franrodriguez123@users.noreply.github.com>
2023-02-23 21:26:59 +0000

    Código necesarios para actualizar el orden de elementos

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -39,3 +39,33 @@ document.querySelectorAll('select[multiple]').forEach(element => {
 		removeItemButton: true,
 	});
 });
+
+document.querySelectorAll('table.sortable').forEach(element => {
+	new Sortable(element.querySelector('tbody'), {
+		animation: 150,
+		onSort: event => {
+			let data = {
+				model: element.dataset.model,
+				data: new Array(),
+			};
+
+			element.querySelectorAll('.id').forEach(item => {
+				data.data.push(item.innerHTML);
+			});
+
+			axios.put(element.dataset.sortableListAjaxUpdateUrl, data, {
+                	headers: {
+                    	csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                	}
+				})
+				.then(response => {
+					if(!response.data.success)
+						alert(response.data.details);
+				})
+				.catch(error => {
+					alert('Error: ' + error);
+					console.error(error)
+				});
+		}
+	});
+});
