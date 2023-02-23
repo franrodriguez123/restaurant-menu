0308db9883d33a927d69be2ba55b233e1be6933f
franrodriguez123 <118189594+franrodriguez123@users.noreply.github.com>
2023-02-23 21:28:17 +0000

    Actualización de dependencias

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: [
                 'resources/css/app.scss',
@@ -18,7 +20,8 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            vue: 'vue/dist/vue.esm-bundler.js',
         }
     }
 });
